b437415ce54d5a694e26a806d29bec64fa6abe7d
Reshma Neerukonda <reshma_n8@yahoo.com>
2016-09-02 14:39:25 +1000

    Added user defaults and collaborations queue

--- modules/custom/collabco_home/privacy-policy-template.tpl.php
+++ modules/custom/collabco_home/privacy-policy-template.tpl.php
@@ -17,7 +17,6 @@ color:white;
 <div class="privacy">
   <!-- Trigger the modal with a button -->
   
-
   <!-- Modal -->
   <div class="modal fade" id="myModal" role="dialog">
     <div class="modal-dialog modal-lg">
@@ -33,109 +32,9 @@ color:white;
 
 <p>This site is hosted and maintained by Collabforge Pty Limited (the Group). &nbsp;The Group understands the importance of protecting the privacy of your personal information. In handling your personal information, the Group is required to comply with the <em>Privacy Act 1988 </em>(Cth) (the Privacy Act) and the Australian Privacy Principles (the APPs).</p>
 
-<p>Further information about the Privacy Act is available on the <a href="https://www.oaic.gov.au/">Office of the Australian Information Commissioner’s website</a>.</p>
-
-<p><strong>Rights to privacy&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</strong></p>
-
-<p>The Group understands the importance of protecting individuals’ rights to privacy. We have prepared this statement to help you understand how we aim to protect the privacy of your personal information. In handling personal information, the Group is subject to the Privacy Act and complies with the APPs. This policy describes the way the Group collects, uses and discloses your personal information and your rights in relation to our management of your personal information. This policy may be updated from time to time.</p>
-
-<p><strong>What kinds of personal information does the Group collect?</strong></p>
-
-<p>Generally, the Group will collect personal information, such as your name and contact details including organisational address, e-mail address and any other information, which is necessary to provide services to you.</p>
-
-<p><strong>Collection of personal information</strong></p>
-
-<p>Generally, the Group collects your personal information directly from you by requesting that you complete an application form through the registration process.</p>
-
-<p><strong>Why does the Group need personal information?</strong></p>
-
-<p>The Group collects this information for the purposes of:</p>
-
-<p>●&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Enabling you to enter into discussions in members’ sections of the site</p>
-
-<p>●&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Enabling you to contact other site members</p>
-
-<p>●&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; In order for you to be identified and contacted by other site members</p>
-
-<p>●&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; In order for us to understand your interests and invite you to be involved in relevant events</p>
-
-<p>●&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; In order for us to allow you into collaborative working spaces</p>
-
-<p>●&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; In order to understand the demographics of parties interested in innovation collectively</p>
-
-<p>A failure to provide your personal information may affect our ability to provide our range of products and services to you.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
-
-<p><strong>Sharing information with other organisations&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</strong></p>
-
-<p>The types of organisations to which the Group may disclose the personal information you provide include:&nbsp;&nbsp;</p>
-
-<p>●&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Australian Federal Government</p>
-
-<p>●&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Other government authorities when and to the extent required by law, and</p>
-
-<p>●&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; The Group’s professional advisers (including legal and accounting firms, auditors, consultants and other advisers). Such disclosure will only be done with your consent and on a confidential basis.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
-
-<p><strong>Security of your personal information&nbsp;&nbsp;&nbsp;</strong></p>
-
-<p>The Group will take all reasonable steps to ensure that the personal information it holds is protected against misuse, loss, unauthorised access, modification or disclosure. Information&nbsp;is held on secure servers or in storage located in controlled environments. The Group’s employees are required to maintain the confidentiality of any personal information held.&nbsp;&nbsp;</p>
-
-<p><strong>Can you access personal information the Group holds about you?</strong></p>
-
-<p>You have the right to access the personal information the Group collects and holds about you. If at any time you would like to access your details or you would you like more information on our approach to privacy, please tell us.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
-
-<p><strong>Changing information</strong></p>
-
-<p>If you want to change your personal details held by the Group, where you are not able to edit these directly, please let us know in writing.</p>
-
-<p><strong>Changes to the Privacy Policy</strong></p>
-
-<p>The Privacy Policy may be updated at any time. If the changes will impact your privacy, we will notify you of the changes.</p>
-
-<p>&nbsp;</p>
-
-<h3><strong>HOW TO CONTACT US</strong></h3>
-
-<p>To assist you with any inquiries, complaints or feedback you may have, please contact our Privacy Compliance Officer at:</p>
-
-<p>Privacy Compliance Officer Collabforge 2/673 Bourke Street, Melbourne VIC 3000</p>
-
-<p>Phone: +61 3 9663 7310</p>
-
-<p>E-mail: contact@collabforge.com</p>
-
-<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
-
-<p><strong>PRIVACY COMPLAINTS</strong></p>
-
-<p>Please direct all privacy complaints to our Privacy Compliance Officer. A privacy complaint relates to any concern or dispute that you have with our privacy practices as it relates to your personal information. This could include matters such as:</p>
-
-<p>(a) How your personal information is collected.</p>
-
-<p>(b) How your personal information is stored.</p>
-
-<p>(c) How your personal information is used or disclosed.</p>
-
-<p>(d) How access to your personal information is provided.</p>
-
-<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
-
-<p>At all times, privacy complaints will:</p>
-
-<p>(a) &nbsp;be treated seriously</p>
-
-<p>(b)&nbsp; be dealt with promptly</p>
-
-<p>(c)&nbsp; be dealt with in a confidential manner, and</p>
-
-<p>(d)&nbsp; not effect your existing obligations to the commercial arrangements that exist between the Group and you.</p>
-
-<p>&nbsp;</p>
-
-<p>The Privacy Compliance Officer will commence an investigation into your complaint. You will be informed of the outcome of your complaint following the completion of the investigation. In the event you are dissatisfied with the outcome of your complaint, you may refer the complaint to the Federal office of the Privacy Commissioner.</p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
+<div class="modal-footer">
+    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+</div>
       </div>
     </div>
   </div>
