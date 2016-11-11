49d0ace32b84a018009bee713045d9a615427cbd
Matt Withoos <mattwithoos@gmail.com>
2016-11-11 11:19:14 +1100

    initial labels commit

--- modules/custom/collabco_challenge/header-challenge-block-template.tpl.php
+++ modules/custom/collabco_challenge/header-challenge-block-template.tpl.php
@@ -5,10 +5,10 @@
  */
 ?>
 
-<div data-toggle="popover"  data-html="true" data-placement="right" data-content="<p>Challenges are a call to action to solve specific problems in our workspace. Sponsored Challenges will be time limited and driven by a Senior Executive.</p>
-<p>By Responding to challenges you enter the innovation process, and contribute to making our work even better! This is wher eyou can create ideas, support ideas and find inspiration.</p>
+<div data-toggle="popover"  data-html="true" data-placement="right" data-content="<p><?php echo ucfirst(variable_get('csl_challenges')); ?>s are a call to action to solve specific problems in our workspace. <?php echo ucfirst(variable_get('csl_sponsor')); ?>ed <?php echo ucfirst(variable_get('csl_challenges')); ?>s will be time limited and driven by a Senior Executive.</p>
+<p>By responding to <?php echo variable_get('csl_challenges'); ?>s you enter the innovation process, and contribute to making our work even better! This is where you can create <?php echo variable_get('csl_ideas'); ?>s, <?php echo variable_get('csl_support'); ?> <?php echo variable_get('csl_ideas'); ?>s and find inspiration.</p>
 <i class='icon-flag'></i>" >
-<h2 class="icon-flag">Challenges<span class="hidden-xs pop"></span></h2>
+<h2 class="icon-flag"><?php echo ucfirst(variable_get('csl_challenges')); ?>s<span class="hidden-xs pop"></span></h2>
 
 <p>Help solve our organisations's big issues.</p>
 
