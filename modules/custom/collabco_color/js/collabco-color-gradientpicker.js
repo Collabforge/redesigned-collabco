6e023e790ec28b7f30010efb84177234a81716fc
Alvaro <alvaro.villafane-molina@collabforge.com>
2017-01-24 14:33:54 +1100

    Updated js on collabco_color module

--- modules/custom/collabco_color/js/collabco-color-gradientpicker.js
+++ modules/custom/collabco_color/js/collabco-color-gradientpicker.js
@@ -2,31 +2,134 @@
  * Provides a gradient picker for the widget.
  */
 (function ($) {
+
+  "use strict"
+
   Drupal.behaviors.collabco_color_gradient = {
     attach: function (context) {
-     
 
-      $('.form-type-collabco-color-gradient-picker input').focusin(function(event) {
-        $(this).parent().siblings('.collabco-gradient-picker').show();
-        console.log('focus in');
-      });
+      var gradientInput = $('.form-type-collabco-color-gradient-picker input');
+      // gradientInput.attr('readonly', true);
 
-      $('.form-type-collabco-color-gradient-picker input').focusout(function(event) {
-        $(this).parent().siblings('.collabco-gradient-picker').hide();
-        console.log('focus out');
-      });
+      var pageChallenge = 'challenge';
+      var pageCollaboration = 'collaboration';
 
-
-      // $("#ex1").gradientPicker({
-      //   change: function(points, styles) {
-      //     for (i = 0; i < styles.length; ++i) {
-      //       console.log("background-image", styles[i]);
-      //     }
-      //   },
-      //   fillDirection: "45deg",
-      //   controlPoints: ["green 0%", "yellow 50%", "green 100%"]
-      // });
+      gradientPicker(pageChallenge);
+      gradientPicker(pageCollaboration);
 
     }
+
   }
+
+    function gradientPicker(pageType) {
+
+        // Init Gradient Picker
+        var formItemGradient = $(".form-item-" + pageType + "-gradient");
+        var formItemGradientInput = $(".form-item-" + pageType + "-gradient input");
+        var formItemGradientInputValue = formItemGradientInput.val();
+        var formItemGradientWebkitInput =  $("#edit-" + pageType + "-webkit-gradient");
+        var formItemGradientBackground = $("#edit-" + pageType + "-background-color");
+        
+        try {
+          var gradientParsed = GradientParser.parse(formItemGradientInputValue);
+        } catch (e) {
+          console.error(e);
+        }
+
+
+        
+        var linearGradientParsed  = getParsedLinearGradient(JSON.stringify(gradientParsed, null, 4));
+
+        formItemGradient.siblings('.grad_ex').gradientPicker({
+          change: function(points, styles) {
+            formItemGradientInput.val(styles[0]);
+            formItemGradientWebkitInput.val('-webkit-' + styles[0]);
+            formItemGradientBackground.val(getFirstColor(styles[0]));
+            console.log('webkit ejemplo: ' + styles[1]);
+
+            for (var i = 0; i < styles.length; ++i) {
+              formItemGradient.siblings('.prev').css("background-image", styles[i]);
+            }
+          },
+          fillDirection: linearGradientParsed.fillDirection,
+          controlPoints: linearGradientParsed.controlPoints
+        });
+
+  }
+
+
+
+  function getParsedLinearGradient(gradientParsed) { 
+    var linearGradient = new Object();
+    linearGradient.controlPoints = [];
+
+    try {
+       var gradientParsed = JSON.parse(gradientParsed);
+       var index = 0;
+
+
+      if( index < gradientParsed.length ) {
+        var gradient = gradientParsed[index];
+
+        for(var i = 0; i < gradient.colorStops.length; i++) {
+          var colorStops = gradient.colorStops[i];
+          var colorType = colorStops.type;
+          var color = getColorByColorType(colorType, colorStops);
+
+          if( color ) {
+            linearGradient.controlPoints[i] = color + colorStops.length.value + colorStops.length.type;
+          }
+
+          if( i == 0 ) {
+            linearGradient.firstColor = color; 
+          }
+        }
+
+        linearGradient.fillDirection = gradient.orientation.value;
+
+        if (gradient.orientation.type == 'angular') {
+          linearGradient.fillDirection += 'deg';
+        }
+
+      }
+    
+      return linearGradient;
+    }
+    catch (e) {
+       console.error(e)
+    }
+  }  
+
+
+  function getFirstColor(gradient) {
+
+    var gradientParsed = JSON.parse(JSON.stringify(GradientParser.parse(gradient), null, 4));
+    var firstColor = '';
+    var indexZero = 0;
+
+    if( gradientParsed.length > indexZero ) {
+      var colorStops = gradientParsed[indexZero].colorStops[indexZero];
+      firstColor = getColorByColorType(colorStops.type, colorStops)
+    }
+    
+    return firstColor;
+
+  }
+
+  function getColorByColorType(colorType, colorStops) {
+    var color = '';
+
+    switch(colorType) {
+      case 'rgb':
+        color = colorType + '(' + colorStops.value[0] + ',' + colorStops.value[1] + ',' + colorStops.value[2] + ') ';
+        break;
+      case 'rgba':
+        color = colorType + '(' + colorStops.value[0] + ',' + colorStops.value[1] + ',' + colorStops.value[2] + ',' + colorStops.value[3] +  ') ';
+        break;
+    }
+
+    return color;
+  }
+
+
 })(jQuery);
