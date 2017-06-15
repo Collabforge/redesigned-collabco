b6dd1d8e86321193cb1c54039de5cb3eaed48569
Alvaro <alvaro.villafane-molina@collabforge.com>
2017-06-15 12:33:44 +1000

    Minor slideshow fix

--- themes/custom/collabco_theme/gulp/gulpfile.js
+++ themes/custom/collabco_theme/gulp/gulpfile.js
@@ -60,8 +60,8 @@ gulp.task('reload', ['clearcache'], function () {
 gulp.task('browser-sync', function() {
       browserSync.init({
       	open: 'external',
-      	host: 'govims.dev',
-      	proxy: 'govims.dev',
+      	host: 'example.dev',
+      	proxy: 'example.dev',
       	port: 8080 // for work mamp
       });
 });
